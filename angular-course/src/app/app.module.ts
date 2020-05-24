3b16941bf226ffbb8a92a2fd858047955a7c3a54
Sergio Fontecha <sergio.fontecha@MacBook-Pro-de-admin-2.local>
2020-05-24 12:46:17 +0200

    components, create and manage @Component, constructor, ngOnInit, variables, types, classes, data models and objects

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FrutaComponent } from 'src/app/components/fruta/fruta.component';
+import { EmpleadosComponent } from 'src/app/components/empleados/empleados.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FrutaComponent, EmpleadosComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-course/src/app/components/empleados/empleados.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleados',
+  templateUrl: './empleados.component.html',
+  styleUrls: ['./empleados.component.scss']
+})
+export class EmpleadosComponent implements OnInit {
+  public nombre_componente = 'Componente emplados';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
