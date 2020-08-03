260a7dd565c271f51d81507d5047f950c2448e4a
Sergio Fontecha <sergio.fontecha@MacBook-Pro-de-admin-2.local>
2020-08-03 14:44:30 +0200

    router imports and paths configuration

--- angular-course/src/app/app-routing.module.ts
+++ angular-course/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Import Componenets
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { FrutaComponent } from './components/fruta/fruta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmpleadosComponent },
+  { path: 'empleados', component: EmpleadosComponent },
+  { path: 'fruta', component: FrutaComponent },
+  { path: '**', component: EmpleadosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
